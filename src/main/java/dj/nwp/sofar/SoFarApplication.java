fc8316f726cf7d1cd060a3a11abc034c826f3081
danilojoncic <djoncic14122rn@raf.rs>
2025-02-28 16:28:54 +0100

    project init

---
+++ src/main/java/dj/nwp/sofar/SoFarApplication.java
@@ -0,0 +1,13 @@
+package dj.nwp.sofar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoFarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoFarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dj/nwp/sofar/SoFarApplicationTests.java
@@ -0,0 +1,13 @@
+package dj.nwp.sofar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoFarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
